322d75cd8b0c013068c4469e9f4445b671e26c09
haaamza <hamzagee@outlook.com>
2022-11-10 12:15:15 +0000

    Build: (23f6906) Merge pull request #1 from Flashgroupdevs/production
    Create publish.yml

---
+++ static/js/678.a063fe6f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_minimal_minimal_kit_react=self.webpackChunk_minimal_minimal_kit_react||[]).push([[678],{9678:function(i,a,e){e.r(a);var m=e(1258);a.default=m.g}}]);
+//# sourceMappingURL=678.a063fe6f.chunk.js.map
